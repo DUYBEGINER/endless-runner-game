35b158ec517221a137f7cfd5590ae050affe7439
Duy <you@example.com>
2024-09-13 01:45:06 +0700

    fix highest score

--- Main/update_high_score.py
+++ Main/update_high_score.py
@@ -3,5 +3,6 @@ import os
 
 def update_score():
     if Variables.score > Variables.high_score:
+        Variables.high_score = Variables.score
         fo = open(os.path.join(Variables.current_dir, 'Main/High_Score'), 'w')
         fo.write(f'{Variables.score}')
